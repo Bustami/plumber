bae5f9c0451fa0ce31e56bafa5ae4c15bfd19175
Ismael Gómez <ismaelgomezs@gmail.com>
2020-08-08 20:45:14 -0400

    Update digital-ocean.R

--- R/digital-ocean.R
+++ R/digital-ocean.R
@@ -11,7 +11,7 @@ checkAnalogSea <- function() {
 
   suggests <- read.dcf(system.file("DESCRIPTION", package = "plumber"))[1, "Suggests"]
   pkgs <- strsplit(suggests, ",")[[1]]
-  analogsea_version <- gsub("[^.0-9]", "", pkgs[grepl("^analogsea ", pkgs)])
+  analogsea_version <- gsub("[^.0-9]", "", pkgs[grepl("^ ?analogsea ", pkgs)])
   if (utils::packageVersion("analogsea") < package_version(analogsea_version)) {
     stop("The analogsea package is not high enough. Please update `analogsea`.",
          call. = FALSE)
